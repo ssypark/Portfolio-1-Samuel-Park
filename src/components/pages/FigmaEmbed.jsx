726167c38592cd85c1a41a8109f4101f87f0c92d
hobbybill <sungyool.park@gmail.com>
2025-03-03 20:50:31 -0800

    flydex update

--- src/components/pages/FigmaEmbed.jsx
+++ src/components/pages/FigmaEmbed.jsx
@@ -7,7 +7,7 @@ const FigmaEmbed = () => {
                 style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
                 width="800"
                 height="800"
-                src="https://embed.figma.com/proto/gCVW1snYrr66F7o51z2MCb/Flydex?page-id=1136%3A1151&node-id=1136-1152&node-type=canvas&viewport=494%2C647%2C0.17&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1136%3A1152&embed-host=share"
+                src="https://www.figma.com/proto/gCVW1snYrr66F7o51z2MCb/Flydex?page-id=1237%3A1002&node-id=1237-1003&p=f&viewport=-1013%2C857%2C0.61&t=rUMsKMUYNatJdhAC-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1237%3A1003&embed-host=share"
                 allowFullScreen
                 title="FlyDex Wireframe"
             ></iframe>
